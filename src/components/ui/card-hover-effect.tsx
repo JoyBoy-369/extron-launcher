47fe87033585dbc556f6bb2fcee47c602f544566
kevin n <kevin.nin92@gmail.com>
2024-07-02 21:29:32 +0530

    background color

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -46,7 +46,7 @@ export const HoverEffect = ({
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-slate-800 dark:bg-slate-800/[0.8] block  rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -112,7 +112,12 @@ export const CardTitle = ({
   children: React.ReactNode;
 }) => {
   return (
-    <h4 className={cn(" text-center font-bold tracking-wide mt-4", className)}>
+    <h4
+      className={cn(
+        " text-center text-white dark:text-white  font-bold tracking-wide mt-4",
+        className,
+      )}
+    >
       {children}
     </h4>
   );
